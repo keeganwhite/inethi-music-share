23d30f3ef3251ab60b5063cf812087ca6b190bff
Keegan White <keeganthomaswhite@gmail.com>
2021-09-14 11:27:26 +0200

    Updated python version and dependencies

--- app/app/api/api.py
+++ app/app/api/api.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from .endpoints import user  # noqa
-from .endpoints import coupon # noqa
-from .endpoints import download # noqa
-from .endpoints import product # noqa
+from endpoints import user  # noqa
+from endpoints import coupon # noqa
+from endpoints import download # noqa
+from endpoints import product # noqa
--- app/app/main.py
+++ app/app/main.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 app = Flask(__name__)
 # Setup app
-from .core import app_setup  # noqa
+from core import app_setup  # noqa
 
 if __name__ == "__main__":
     # Only for debugging while developing
